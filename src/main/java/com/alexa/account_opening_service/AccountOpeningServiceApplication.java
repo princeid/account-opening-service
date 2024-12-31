8179ff183df240af0ddd8111efffb8b0fa578284
princedi <dicksoniprince@gmail.com>
2024-12-31 14:10:59 +0100

    Project setup

---
+++ src/main/java/com/alexa/account_opening_service/AccountOpeningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexa.account_opening_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOpeningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOpeningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexa/account_opening_service/AccountOpeningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexa.account_opening_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountOpeningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
